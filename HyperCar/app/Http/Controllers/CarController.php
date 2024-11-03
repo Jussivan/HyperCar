ff226c7475befadd261a7aa88d2b1f9f78cc8028
27 <jussivanbm1029@gmail.com>
2024-11-03 13:37:38 -0300

    crud

--- HyperCar/app/Http/Controllers/CarController.php
+++ HyperCar/app/Http/Controllers/CarController.php
@@ -7,59 +7,54 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $cars = Car::all();
+        return view('cars.index', compact('cars'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('cars.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'model' => 'required|string|max:255',
+            'year' => 'required|integer|min:1900|max:' . date('Y'),
+        ]);
+
+        Car::create($request->all());
+        return redirect()->route('cars.index')->with('success', 'Carro criado com sucesso!');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Car $car)
     {
-        //
+        return view('cars.show', compact('car'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Car $car)
     {
-        //
+        return view('cars.edit', compact('car'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Car $car)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'model' => 'required|string|max:255',
+            'year' => 'required|integer|min:1900|max:' . date('Y'),
+        ]);
+
+        $car->update($request->all());
+        return redirect()->route('cars.index')->with('success', 'Carro atualizado com sucesso!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Car $car)
     {
-        //
+        $car->delete();
+        return redirect()->route('cars.index')->with('success', 'Carro removido com sucesso!');
     }
 }
